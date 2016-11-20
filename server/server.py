9ca1e6f041ad2ac7f140b5898cf79979dff817b4
Teodor Stefu <Teodorstefu@gmail.com>
2016-11-20 21:30:49 +0200

    added server api

---
+++ server/server.py
@@ -0,0 +1,54 @@
+from flask import Flask, jsonify
+
+
+class HouseMonitoringServer(object):
+    def __init__(self):
+        super(HouseMonitoringServer, self).__init__()
+        self.electricity_base = 20
+        self.hot_water_base = 2
+        self.cold_water_base = 1
+        self.temperature_base = 28
+        self.gas_base = 3
+        self.app = Flask(__name__)
+
+
+
+
+if __name__ == "__main__":
+
+    app = Flask(__name__)
+    serverConfigs = HouseMonitoringServer()
+
+    @app.route('/sensors/electricity')
+    def sensor_electricity():
+        response = {'electricity': serverConfigs.electricity_base}
+        response = jsonify(response)
+        response.status_code = 200
+        return response
+
+    @app.route('/sensors/water')
+    def sensor_water():
+        response = {
+            'hot_water': serverConfigs.hot_water_base,
+            'cold_water': serverConfigs.cold_water_base
+        }
+        response = jsonify(response)
+        response.status_code = 200
+        return response
+
+    @app.route('/sensors/temparature')
+    def sensor_temperature():
+        response = {'electricity': serverConfigs.temperature_base}
+        response = jsonify(response)
+        response.status_code = 200
+        return response
+
+    @app.route('/sensors/gas')
+    def sensor_gas():
+        response = {'gas': serverConfigs.gas_base}
+        response = jsonify(response)
+        response.status_code = 200
+        return response
+    
+
+    app.run(threaded=True, port=8080)
